a64b6c8326d9c20defc004931fcf4922b2b744ba
Abbie Tolon (She/Her) <59490033+artolon@users.noreply.github.com>
2022-09-21 10:15:01 -0500

    Update streamlit_app.py
    made the picklist table active and workable

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,12 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # adding a pick list here so people can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# pre-populate the list, so that it is more obvious to customers
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
 
 
