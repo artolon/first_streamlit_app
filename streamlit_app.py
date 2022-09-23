1e26bad543762cd00aed26518dc43db32b129e2c
Abbie Tolon (She/Her) <59490033+artolon@users.noreply.github.com>
2022-09-23 11:21:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,7 @@ streamlit.dataframe(fruits_to_show)
 
 # new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # make data readable
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
